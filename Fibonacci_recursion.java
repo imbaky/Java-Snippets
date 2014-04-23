b1b93474d9807044889cb9970f2b2bbf586db518
membug <mo7_abd@hotmail.com>
2014-04-23 15:12:59 -0400

    Create Fibonacci_recursion.java

---
+++ Fibonacci_recursion.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+public class driver {
+	
+	static private int count;
+	
+
+	static void fab(int x, int y){
+	int temp;
+	if (x>y){
+		temp=x; 
+		x=y;
+		y=temp;
+	}
+	count++;
+	if(count==20)return;
+	if(count==1) System.out.print(" "+x+" "+y);
+	System.out.print(" "+(x+y));
+
+	fab(y, (x+y));
+	}
+	
+
+
+ public static void main(String[] args) {
+	 Scanner keyboard=new Scanner(System.in);
+count=0;
+System.out.print("\t\t Welcome to my Fibonacci program \n\n");
+System.out.print("Which two numbers would you like to start from?");
+	 fab(keyboard.nextInt(),keyboard.nextInt());
+	 
+	}
+
+}
